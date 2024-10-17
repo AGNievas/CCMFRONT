dae694c5d22dc72ef3c6c56aab1f29d4b344f6e0
humbledragon <humbledragon.ia@gmail.com>
2024-10-17 19:56:23 -0300

    se crea stockAreaService

---
+++ src/componentes/servicios/stockAreaService.js
@@ -0,0 +1,84 @@
+
+
+import axiosInstance from "@/axios.js";
+
+const URL = '/stock-area'; // URL base
+
+
+
+const stockAreaService = {
+
+  async getAllStockArea() {
+    try {
+      const response = await axiosInstance.get(URL,{withCredentials:true}) ;
+      return response.data.return;
+    } catch (error) {
+      console.error("Error al obtener stockArea:", error);
+      return [];
+    }
+  },
+
+
+  async  getStockAreaById(id) {
+    try {
+      console.log("crearStockAreaPostResponse")
+      const response = await axiosInstance.get(`${URL}/id/${id}`,{withCredentials:true});
+      return response.data.return;
+    } catch (error) {
+      console.error("Error al obtener stockArea:", error);
+      return [];
+    }
+  },
+
+  async  getStockAreaByNombre(nombre) {
+    try {
+      console.log("crearStockAreaPostResponse")
+      const response = await axiosInstance.get(`${URL}/nombre/${nombre}`,{withCredentials:true});
+      return response.data.return;
+    } catch (error) {
+      console.error("Error al obtener stockArea:", error);
+      return [];
+    }
+  },
+
+  async  createStockArea(nombre) {
+    try {
+      console.log("crearStockAreaPostResponse")
+      const response = await axiosInstance.post(`${URL}`,{nombre},{withCredentials:true});
+      console.log("crearStockAreaPostResponse",response.data.return)
+      return response.data.return;
+    } catch (error) {
+      console.error("Error al obtener stockArea:", error);
+      return [];
+    }
+  },
+
+ 
+  async  updateStockArea(id, nombre) {
+    try {
+      console.log("antesUpdateStockArea", sku,descripcion,tipoInsumo)
+      const {data: StockAreaActualizado} = await axiosInstance.put(`${URL}/${id}`, {nombre},{withCredentials:true});
+      console.log("despuesUpdateStockArea", StockAreaActualizado)
+      return StockAreaActualizado;
+    } catch (error) {
+      console.error("Error al actualizar el usuario:", error);
+      throw error;
+    }
+  },
+
+  async  deleteStockArea(id) {
+    try {
+      console.log("antesDeleteStockArea", id)
+      const {data: StockAreaEliminado} = await axiosInstance.delete(`${URL}/${id}`,{withCredentials:true});
+      return StockAreaEliminado;
+    } catch (error) {
+      console.error("Error al actualizar el usuario:", error);
+      throw error;
+    }
+  }
+
+}
+
+
+
+export default stockAreaService;
