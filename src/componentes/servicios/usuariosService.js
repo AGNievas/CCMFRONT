493d94690080ec1b1cb5389ba5c92429718ea6f3
Aggux <agustina155.nievas@gmail.com>
2024-11-07 22:34:25 -0300

    fix

--- src/componentes/servicios/usuariosService.js
+++ src/componentes/servicios/usuariosService.js
@@ -26,8 +26,10 @@ const usuariosService = {
   async getUsuarioById(id) {
     try {
 
-      const response = await axiosInstance.get(`${URL}/id/${id}`, {withCredentials: true,});
-      return response.data.return;
+      const response = await axiosInstance.get(`${URL}/id/${id}`, {
+        withCredentials: true,
+      });
+      return response.data.message;
     } catch (error) {
       console.error(`Error al obtener el User id: ${id} `, error);
       throw error;
