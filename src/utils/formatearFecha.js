b3142abc27b22288747b1b462b2e73ec57bd93c7
Aggux <agustina155.nievas@gmail.com>
2024-11-10 18:40:59 -0300

    fix

--- src/utils/formatearFecha.js
+++ src/utils/formatearFecha.js
@@ -2,6 +2,6 @@ export function formatearFecha(fecha) {
   const date = new Date(fecha);
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
+  const day = String(date.getDate() + 1).padStart(2, '0');
   return `${year}-${month}-${day}`;
 }
