260c79a84e5762c4efb99d76f7db10aa48118eb6
Aggux <agustina155.nievas@gmail.com>
2024-11-12 18:44:13 -0300

    fixed

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack');
+
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  devServer:{
+  devServer: {
     hot: false,
     liveReload: true,
-    }
-})
+  },
+
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: JSON.stringify(true),
+        __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
+      }),
+    ],
+  },
+});
